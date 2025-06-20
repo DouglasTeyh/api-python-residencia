c6ebf9d3ef883066c7fa6f3d0ff34285089c1b4a
DouglasTeyh <contato.douglasteyh@gmail.com>
2025-06-20 15:59:42 -0300

    preparando rotas

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
 from flask import request, jsonify
 from app import app
+from app.controllers import adicionar_usuario, listar_usuarios, deletar_usuario, atualizar_usuario
 
+#rota saudacao
 @app.route('/saudacao', methods=['GET'])
 def saudacao():
     return jsonify({"mensagem": "Bem-vindo à API de exemplo"})
+
+#Rota para adicionar um usuário
+@app.route('/usuarios', methods=['POST'])
+def criar_usuario():
+    '''a fazer'''
+
+#rota para listar usuários
+@app.route('/usuarios', methods=['GET'])
+def obter_usuarios():
+    '''a fazer'''
+
+#rota para deletar um usuário
+@app.route('/usuarios/<int:id>', methods=['DELETE'])
+def deletar_usuario(id):
+    '''a fazer'''
+
+#rota para atualizar um usuário
+@app.route('/usuarios/<int:id>', methods=['PUT'])
+def atualizar_usuario(id):
+    '''a fazer'''
