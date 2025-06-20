88710ef66732844d9ce0bd5a393d141cf753c1e2
DouglasTeyh <contato.douglasteyh@gmail.com>
2025-06-20 17:48:50 -0300

    Refatoração #2

--- app/controllers.py
+++ app/controllers.py
@@ -2,21 +2,32 @@ from app.models import Usuario
 from app.data import usuarios
 
 def adicionar_usuario(nome, email):
-    id = len(usuarios) + 1
-    novo_usuario = Usuario(id, nome, email)
+    novo_id = len(usuarios) + 1
+    novo_usuario = Usuario(novo_id, nome, email)
     usuarios.append(novo_usuario)
     return novo_usuario
 
 def listar_usuarios():
     return [usuario.to_dict() for usuario in usuarios]
 
-def deletar_usuario(id):
+def deletar_usuario(usuario_id):
     global usuarios
-    usuarios = [usuario for usuario in usuarios if usuario.id != id]
+    usuario_encontrado = None
 
-def atualizar_usuario(id, nome, email):
     for usuario in usuarios:
-        if usuario.id == id:
+        if usuario.id == usuario_id:
+            usuario_encontrado = usuario
+            break
+
+    if usuario_encontrado:
+        usuarios = [usuario for usuario in usuarios if usuario.id != usuario_id]
+        return True
+    else:
+        return False
+
+def atualizar_usuario(usuario_id, nome, email):
+    for usuario in usuarios:
+        if usuario.id == usuario_id:
             usuario.nome = nome
             usuario.email = email
             return usuario
