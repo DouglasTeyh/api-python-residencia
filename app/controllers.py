6e5e0bbf284a37a0857e528e89e8eb988ec6ed4a
DouglasTeyh <contato.douglasteyh@gmail.com>
2025-06-20 15:23:43 -0300

    controlers adicionados: adicionar/listar users

--- app/controllers.py
+++ app/controllers.py
@@ -1,2 +1,11 @@
 from app.models import Usuario
 from app.data import usuarios
+
+def adicionar_usuario(nome, email):
+    id = len(usuarios) + 1
+    novo_usuario = Usuario(id, nome, email)
+    usuarios.append(novo_usuario)
+    return novo_usuario
+
+def listar_usuarios():
+    return [usuario.to_dict() for usuario in usuarios]
